79de0fc6a7f8ed8bbde096437db6fecaa1918ca1
Spyke Lionel <social.lionel@gmail.com>
2024-08-13 07:11:57 +0100

    update to use cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const middleware = require("./middleware");
 
 app.use(express.json());
-
+app.use(cors({}));
 const PORT = 4000;
 
 // post request
